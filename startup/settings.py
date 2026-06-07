cc9c84f040802b4c6ccd5a339c49c7f7326b706d
Dev Local <dev@local>
2026-06-08 00:13:57 +0530

    production settings for deployment

--- startup/settings.py
+++ startup/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = 'django-insecure-d)p2b#0(%d!6z9c&yb_%3^c=pj_@c+vcd8ni#c8s6@e1^44dsb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "mdmubeen001.pythonanywhere.com",
+]
 
 
 # Application definition
