e806339f5e0d1177f693faeedf1c2cbe1a350386
Mohammed Muzaffar <nohammedmuzaffar910@gmail.com>
2026-06-07 19:59:44 +0530

    Fix static files and merge migrations

--- startup/settings.py
+++ startup/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR /  'static',
 ]
-#STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGIN_URL = 'login'
